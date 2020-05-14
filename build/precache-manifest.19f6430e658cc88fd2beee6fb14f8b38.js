69394575f7421d9537a9f62097568d32d05f5557
Myron Huang <myron.h93@gmail.com>
2020-05-13 23:22:49 -0400

    ran npm run build

---
+++ build/precache-manifest.19f6430e658cc88fd2beee6fb14f8b38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296b00355178535e5195a6833de37cd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "719f09f6547579420593",
+    "url": "/static/css/main.b380fce6.chunk.css"
+  },
+  {
+    "revision": "f22045af0184939fa1dd",
+    "url": "/static/js/2.65758bd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65758bd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "719f09f6547579420593",
+    "url": "/static/js/main.da75e125.chunk.js"
+  },
+  {
+    "revision": "9e4b0d9cbe501565cf7e",
+    "url": "/static/js/runtime-main.64ac7dab.js"
+  }
+]);
